3f9ac8036cb1aa69dd240160699a98ae2154a668
Domonique Taylor <taylord651@gmail.com>
2020-05-05 18:59:29 -0500

    Outlined steps

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,15 @@ package com.company;
 
 public class Main {
 
+    //feat/4: Create method to calculate area
+
     public static void main(String[] args) {
-	// write your code here
+	// feat/1: Setup the following variables (int length, int width, Scanner input)
+
+
+    // feat/2: Ask user to enter a length and set user entry to the int length variable
+
+
+    // feat/3: Ask user to enter a width and set user entry to the int width variable
     }
 }
