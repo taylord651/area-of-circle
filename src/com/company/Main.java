e0652e6bfa63a94b8a447892fd3b25b9c419c25e
Domonique Taylor <taylord651@gmail.com>
2020-05-05 19:29:22 -0500

    Added length, width, and input variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     //feat/4: Create method to calculate area
 
     public static void main(String[] args) {
 	// feat/1: Setup the following variables (int length, int width, Scanner input)
-
+        int length;
+        int width;
+        Scanner input;
 
     // feat/2: Ask user to enter a length and set user entry to the int length variable
 
